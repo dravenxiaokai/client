01b8ec1397db6e1f7e917d0291f06828254c0b8d
Draven <jspankai@vip.qq.com>
2017-07-24 23:07:42 +0800

    添加websocket与服务端进行通讯

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { StockComponent } from './stock/stock.component';
 import { HttpModule } from '@angular/http';
+import { WebSocketComponent } from './web-socket/web-socket.component';
+import { WebSocketService } from './shared/web-socket.service'
 
 @NgModule({
   declarations: [
     AppComponent,
-    StockComponent
+    StockComponent,
+    WebSocketComponent
   ],
   imports: [
     BrowserModule,
     HttpModule
   ],
-  providers: [],
+  providers: [WebSocketService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
